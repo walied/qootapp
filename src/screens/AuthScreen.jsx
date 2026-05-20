ca0ec58988564a001fd6a0b521e38548bea728b4
walied <wildfarag@gmail.com>
2026-05-20 13:09:12 +0300

    Create AuthScreen.jsx

---
+++ src/screens/AuthScreen.jsx
@@ -0,0 +1,47 @@
+import { useApp, AppProvider } from "./context/AppContext";
+import LandingScreen from "./screens/LandingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import QuizScreen from "./screens/QuizScreen";
+import PlanScreen from "./screens/PlanScreen";
+import DashboardScreen from "./screens/DashboardScreen";
+import GeneratingScreen from "./screens/GeneratingScreen";
+import ErrorScreen from "./screens/ErrorScreen";
+import FollowUpScreen from "./screens/FollowUpScreen";
+import FollowUpPlanScreen from "./screens/FollowUpPlanScreen";
+import CommunityScreen from "./screens/CommunityScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import AuthScreen from "./screens/AuthScreen";
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppRouter />
+    </AppProvider>
+  );
+}
+
+function AppRouter() {
+  const { screen, uid } = useApp();
+
+  // Protect routes that require authentication
+  const requiresAuth = ["dashboard", "community", "profile"];
+  if (requiresAuth.includes(screen) && !uid) {
+    return <AuthScreen />;
+  }
+
+  switch (screen) {
+    case "landing": return <LandingScreen />;
+    case "login": return <LoginScreen />;
+    case "quiz": return <QuizScreen />;
+    case "plan": return <PlanScreen />;
+    case "dashboard": return <DashboardScreen />;
+    case "generating": return <GeneratingScreen />;
+    case "error": return <ErrorScreen />;
+    case "followup": return <FollowUpScreen />;
+    case "followup_plan": return <FollowUpPlanScreen />;
+    case "community": return <CommunityScreen />;
+    case "profile": return <ProfileScreen />;
+    case "auth": return <AuthScreen />;
+    default: return <LandingScreen />;
+  }
+}
