9187e7ec3653d7a257c01da924decf52d862abdb
walied <wildfarag@gmail.com>
2026-05-22 20:56:32 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from './context/AppContext'; // ← import provider
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppProvider>      {/* ← wrap App */}
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
