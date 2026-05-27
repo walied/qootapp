90346b482267d26c67934fa9fa4dc1adcbb5efc8
walied <wildfarag@gmail.com>
2026-05-27 18:27:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import { useApp } from "./context/AppContext";
 import { C } from "./constants";
 import LandingScreen from "./screens/LandingScreen";
-import QuizScreen from "./screens/QuizScreen";
-import GeneratingScreen from "./screens/GeneratingScreen";
-import PlanScreen from "./screens/PlanScreen";
-import ErrorScreen from "./screens/ErrorScreen";
-import FollowUpScreen from "./screens/FollowUpScreen";
-import FollowUpPlanScreen from "./screens/FollowUpPlanScreen";
-import DashboardScreen from "./screens/DashboardScreen";
-import CommunityScreen from "./screens/CommunityScreen";
-import ProfileScreen from "./screens/ProfileScreen";
 import AuthScreen from "./screens/AuthScreen";
-import LoginScreen from "./screens/LoginScreen";
+import AICoachScreen from "./screens/AICoachScreen";
 
 function AppContent() {
   const { screen } = useApp();
-
   switch (screen) {
     case "landing": return <LandingScreen />;
     case "auth": return <AuthScreen />;
-    case "login": return <LoginScreen />;
-    case "quiz": return <QuizScreen />;
-    case "generating": return <GeneratingScreen />;
-    case "plan": return <PlanScreen />;
-    case "error": return <ErrorScreen />;
-    case "followup": return <FollowUpScreen />;
-    case "followup_plan": return <FollowUpPlanScreen />;
-    case "dashboard": return <DashboardScreen />;
-    case "community": return <CommunityScreen />;
-    case "profile": return <ProfileScreen />;
+    case "coach": return <AICoachScreen />;
     default: return <LandingScreen />;
   }
 }
